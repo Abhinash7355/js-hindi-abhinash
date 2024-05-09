b97cbb3158a96f9a95916f733cdb1a3607a44f6e
Abhinash7355 <149595880+Abhinash7355@users.noreply.github.com>
2024-05-09 14:39:44 +0530

    variable_01

---
+++ Abhi/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "abhinash@gmail.com"
+var accountPsaaword ="12345"
+accountCity = "Lucknow"
+
+// accountId = 144553
+accountEmail = "abhinash@gmail.com"
+accountPsaaword ="12345"
+accountCity = "Lucknow"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPsaaword,accountCity])
