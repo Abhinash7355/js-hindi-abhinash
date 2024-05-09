edbdd33894d6029b9e945c53ba84bb1955cd777d
Abhinash7355 <149595880+Abhinash7355@users.noreply.github.com>
2024-05-09 15:10:03 +0530

    Create 01_datatype.js

---
+++ Abhi/01_datatype.js
@@ -0,0 +1,25 @@
+
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using node js , not brower
+
+
+let name ="Abvhinash"
+let age = 18
+let isloggedIn = false
+
+
+// number => 2 to power 53
+//bigint => if number is larged
+
+// string -> ""
+// boolean => true/false
+// null => standalone value or representation of empty value
+//undifined => value not defined
+// symbol =>  unque
+
+
+
+// object 
+console.log(typeof ("Abhi"));
+console.log(typeof age);
