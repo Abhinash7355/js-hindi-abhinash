1143adb8b935d98df241c88ba2fe25b109f0d24d
Abhinash7355 <149595880+Abhinash7355@users.noreply.github.com>
2024-05-20 18:02:18 +0530

    Update 03_conversionandoperation.js

--- Abhi/03_conversionandoperation.js
+++ Abhi/03_conversionandoperation.js
@@ -29,3 +29,46 @@ let stringNumber = String(someNumber)
 
  // *********************** Operations ***********************
  
+ let value = 3
+ let negvalue = -value
+ console.log(negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3); 
+//console.log(2%3); //remainder
+
+
+let str1 = "Hello"
+let str2 = " Abbhinash"
+let str3 = " Gond"
+let str4 = str1 + str2 + str3
+console.log(str4);
+
+console.log("1" + 2);
+console.log("1" + "2");
+console.log(1 + 2);
+console.log(1 + "2");
+console.log("1" + 2 + "2");
+console.log(1 + 2 + "2");
+console.log(1 + 2 * 3);
+
+
+ console.log(+true);
+ console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+ 
+ 
+ 
