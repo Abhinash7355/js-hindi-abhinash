79cfff9600123641f4f08d4f9749875f60d0abc4
Abhinash7355 <149595880+Abhinash7355@users.noreply.github.com>
2024-05-20 10:45:28 +0530

    Create 03_conversionandoperation.js

---
+++ Abhi/03_conversionandoperation.js
@@ -0,0 +1,28 @@
+///let score = "33abc"
+///let score = null
+//let score = true
+let score = "abhinash"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let vlaueInNumber = Number(score)
+console.log(typeof vlaueInNumber);
+console.log( vlaueInNumber);
+//'33'= 33
+//"33abc = NaN
+//true = 1 ; false = 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1= true
+//"" = false
+//"abhi = true"
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
