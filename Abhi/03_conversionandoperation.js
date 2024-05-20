303706446c8c9c5cf024b4b80397d8e3bf346c81
Abhinash7355 <149595880+Abhinash7355@users.noreply.github.com>
2024-05-20 18:11:13 +0530

    Update 03_conversionandoperation.js

--- Abhi/03_conversionandoperation.js
+++ Abhi/03_conversionandoperation.js
@@ -67,8 +67,35 @@ let gameCounter = 100
 console.log(gameCounter);
 
 
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+//Postfix increment
+//let x = 3;
+//const y = x++;
+// x is 4; y is 3
+
+//let x2 = 3n;
+//const y2 = x2++;
+// x2 is 4n; y2 is 3n
 
 
- 
- 
+//Prefix increment
+//let x = 3;
+//const y = ++x;
+// x is 4; y is 4
+
+//let x2 = 3n;
+//const y2 = ++x2;
+// x2 is 4n; y2 is 4n
+
  
